b5f741e8146df4caeec6f4354668aeb2665dcead
sayali gad <gad.sayali@gmail.com>
2020-01-11 20:39:08 +0530

    responsive

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
+// const options = {
+//   root: null, // setting root to null sets it to viewport
+//   rootMargin: "0px",
+//   threshold: 0.99
+// };
+
+// const observerCallback = entries => {
+//   const { isIntersecting, intersectionRatio } = entries[0];
+//   if (isIntersecting === true || intersectionRatio > 0) {
+//     console.log(entries[0].target);
+
+//     // document.scrollingElement.scrollTop = 0;
+//     //   observer.unobserve(entry.target);
+//   }
+// };
+
+// var observer = new IntersectionObserver(observerCallback, options);
+
+// const onload = () => {
+//   const target = document.querySelector(".header--clone");
+//   const aboutTarget = document.querySelector(".about");
+//   const contactTarget = document.querySelector(".contact");
+//   observer.observe(aboutTarget);
+// };
+
 const options = {
-  root: null, // setting root to null sets it to viewport
+  root: null,
   rootMargin: "0px",
-  threshold: 0.99
+  threshold: [1.0, 0.7]
+  // trackVisibility: true
 };
 
-const observerCallback = entries => {
+const callback = entries => {
+  entries.forEach(entry => {
+    const { isIntersecting } = entry;
+    console.log(entry);
+    const target = document.querySelector(".about__text").classList;
+    if (isIntersecting) {
+      target.remove("fade");
+    } else {
+      target.add("fade");
+    }
+  });
+};
+
+const optionsClone = {
+  root: null, // setting root to null sets it to viewport
+  rootMargin: "0px",
+  threshold: [0.99]
+};
+
+const callbackClone = entries => {
   const { isIntersecting, intersectionRatio } = entries[0];
-  if (isIntersecting === true || intersectionRatio > 0) {
-    console.log(document);
-    // document.scrollingElement.scrollTop = 0;
-    //   observer.unobserve(entry.target);
+  if (isIntersecting === true) {
+    document.scrollingElement.scrollTop = 5;
   }
 };
 
-var observer = new IntersectionObserver(observerCallback, options);
+let observer = new IntersectionObserver(callback, options);
+var observerClone = new IntersectionObserver(callbackClone, optionsClone);
 
-const onload = () => {
-  const target = document.querySelector(".header--clone");
-  // observer.observe(target);
+const onLoad = e => {
+  const elem = document.querySelector(".about");
+  const elemClone = document.querySelector(".header--clone");
+  observer.observe(elem);
+  observerClone.observe(elemClone);
 };
